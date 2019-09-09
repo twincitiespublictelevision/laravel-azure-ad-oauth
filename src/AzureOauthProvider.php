28981478f713f3329acca5f47e347f7bcd43bff6
Josef Schabasser <josef.schabasser@salamantex.com>
2019-09-09 14:41:45 +0200

    fix single tenant mode

--- src/AzureOauthProvider.php
+++ src/AzureOauthProvider.php
@@ -15,12 +15,12 @@ class AzureOauthProvider extends AbstractProvider implements ProviderInterface
 
     protected function getAuthUrl($state)
     {
-        return $this->buildAuthUrlFromBase('https://login.microsoftonline.com/' . config('azure_tenant_id', 'common') . '/oauth2/authorize', $state);
+        return $this->buildAuthUrlFromBase('https://login.microsoftonline.com/' . config('azure-oath.azure_tenant_id', 'common') . '/oauth2/authorize', $state);
     }
 
     protected function getTokenUrl()
     {
-        return 'https://login.microsoftonline.com/' . config('azure_tenant_id', 'common') . '/oauth2/token';
+        return 'https://login.microsoftonline.com/' . config('azure-oath.azure_tenant_id', 'common') . '/oauth2/token';
     }
 
     protected function getTokenFields($code)
